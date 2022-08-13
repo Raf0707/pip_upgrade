d7ad2e5d83803b2feea077493871867ccd30c549
Rafail <Raf_0707@mail.ru>
2022-08-13 23:14:04 +0300

    add updatePIP for PyCharm Terminal

--- upgradePIP.py
+++ upgradePIP.py
@@ -10,8 +10,23 @@ label1 = tk.Label(root, text='Upgrade PIP', bg = 'lightsteelblue2')
 label1.config(font=('helvetica', 20))
 canvas1.create_window(150, 80, window=label1)
  
-def upgradePIP ():
-    os.system('start cmd /k python.exe -m pip install --upgrade pip') 
+def upgradePIP():
+
+    '''
+    Выберите вашу среду разработки - PyCharm или IDLE Python
+    P. S. Анаконду не ставил, но попробуйте вариант для PyCharm в анаконде
+    Так как терминалы PyCharm и Anaconda идентичны
+
+    Выберите подходящий вам вариант и оставьте его
+    неподходящий вариант удалите или закомментируйте
+    для однострочного комментария используйте знак # перед началом строки
+    '''
+
+    # for PyCharm
+    os.system('pip install --upgrade pip')
+
+    # for IDLE Python
+    os.system('start cmd /k python.exe -m pip install --upgrade pip')
     
 button1 = tk.Button(text='      Upgrade PIP     ', command=upgradePIP, bg='green', fg='white', font=('helvetica', 12, 'bold'))
 canvas1.create_window(150, 180, window=button1)
